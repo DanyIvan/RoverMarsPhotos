1c67d132529d44a058f2d30aea1acc9d29c15f2f
Daniel <dany_ivan_gr@hotmail.com>
2021-05-13 19:14:07 -0500

    feat: include apod as an option

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -1,14 +1,14 @@
-let store = {
-    user: { name: "Student" },
-    apod: '',
-    rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-}
+let store = Immutable.Map({
+    data: '',
+    currentData: 'Apod',
+    Images: ['Curiosity', 'Opportunity', 'Spirit', 'Apod'],
+})
 
 // add our markup to the page
 const root = document.getElementById('root')
 
 const updateStore = (store, newState) => {
-    store = Object.assign(store, newState)
+    store = store.merge(newState)
     render(root, store)
 }
 
@@ -19,24 +19,40 @@ const render = async (root, state) => {
 
 // create content
 const App = (state) => {
-    let { rovers, apod } = state
 
     return `
-        <header></header>
+        <header>
+            <h2>Space exploration</h2> 
+            <h1>Rovers' recent photos</h1>
+            <nav>
+            <ul id="options"   >
+            <li class="ImageOption">
+                <button class="ImageButton">
+                APOD
+                </button>
+            </li>
+            <li class="ImageOption">
+                <button class="ImageButton">
+                CURIOSITY
+                </button>
+            </li>
+            <li class="ImageOption">
+                <button class="ImageButton">
+                OPPORTUNITY
+                </button>
+            </li>
+            <li class="ImageOption">
+                <button class="ImageButton">
+                SPIRIT
+                </button>
+            </li>
+            </ul>
+        </nav>
+        </header>
+
         <main>
-            ${Greeting(store.user.name)}
             <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
+                ${getData(state)}
             </section>
         </main>
         <footer></footer>
@@ -50,22 +66,15 @@ window.addEventListener('load', () => {
 
 // ------------------------------------------------------  COMPONENTS
 
-// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-    if (name) {
-        return `
-            <h1>Welcome, ${name}!</h1>
-        `
+const getData = (state) => {
+    if (state.get('currentData') === 'Apod'){
+        return ImageOfTheDay(state.get('data'))
     }
-
-    return `
-        <h1>Hello!</h1>
-    `
 }
 
 // Example of a pure function that renders infomation requested from the backend
 const ImageOfTheDay = (apod) => {
-
+    console.log(apod)
     // If image does not already exist, or it is not from today -- request it again
     const today = new Date()
     const photodate = new Date(apod.date)
@@ -73,7 +82,8 @@ const ImageOfTheDay = (apod) => {
 
     console.log(photodate.getDate() === today.getDate());
     if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
+        getImageOfTheDay()
+        console.log(apod)
     }
 
     // check if the photo of the day is actually type video!
@@ -85,8 +95,13 @@ const ImageOfTheDay = (apod) => {
         `)
     } else {
         return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
+            <div class="apodTitle">
+            <h2>Here is the Astronomical Picture of the Day!!</h2>
+            </div>
+            <img src="${apod.get('image').get('url')}" class="apod"/>
+            <div class="apodExp">
+            <p><b>Explanation:</b> ${apod.get('image').get('explanation')}</p>
+            </div>
         `)
     }
 }
@@ -94,12 +109,20 @@ const ImageOfTheDay = (apod) => {
 // ------------------------------------------------------  API CALLS
 
 // Example API call
-const getImageOfTheDay = (state) => {
-    let { apod } = state
-
+const getImageOfTheDay = () => {
     fetch(`http://localhost:3000/apod`)
         .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
+        .then(data => {
+            updateStore(store, { data })
+        })
 
-    return data
 }
+
+// const getImageOfTheDay = (state) => {
+//     fetch(`http://localhost:3000/apod`)
+//         .then(res => res.json())
+//         .then(apod => {
+//             updateStore(store, { apod })
+//         })
+
+// }
